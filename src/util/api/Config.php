c1c490506b9cb16273cd22399865d199e08d9ebf
tonamao <tonamao@tonamao.com>
2022-05-29 00:51:13 +0900

    php, docker系のファイル削除

--- src/util/api/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'score_history' => [
-        'api-server' => 'http://host.docker.internal',
-        'port'       => '8880',
-        'get-uri'    => 'scorehistories',
-    ],
-];
